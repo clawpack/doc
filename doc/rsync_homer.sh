0ce17690790779c159377146156ee920077f0d43
Randy LeVeque <rjl@uw.edu>
2018-06-14 11:49:19 -0700

    script to upload sampledocs

--- doc/rsync_homer.sh
+++
@@ -1,7 +0,0 @@
-
-# Use to sync up files on clawpack.org server:
-
-chmod -R og+rX _build
-rsync -avz --delete _build/html/ \
-  clawpack@homer.u.washington.edu:public_html/v5.4.0alpha/
-
---
+++ doc/rsync_sampledocs.sh
@@ -0,0 +1,7 @@
+
+# Use to sync up files in sampledocs for other developers to view
+
+chmod -R og+rX _build
+rsync -avz --delete _build/html/ \
+  clawpack@homer.u.washington.edu:public_html/sampledocs/v5.5.0alpha/
+
