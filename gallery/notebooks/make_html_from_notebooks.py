af447bbf99c02e379b1f6d1f80db9f8ec77a1d3c
Randy LeVeque <rjl@uw.edu>
2020-05-11 09:01:43 -0700

    Modify make_html_from_notebooks.py to use apps as base location
    Before used apps/notebooks but there might be other apps directories
    with notebooks.

--- gallery/notebooks/make_html_from_notebooks.py
+++ gallery/notebooks/make_html_from_notebooks.py
@@ -1,9 +1,12 @@
 """
-Convert Jupyter notebooks in $CLAW/apps/notebooks directory to html files after
+Convert Jupyter notebooks in $CLAW/apps/ directory to html files after
 executing the notebook.  
 
-The notebooks are put in $CLAW/doc/gallery/notebooks with the same subdirectory
-structure as in $CLAW/apps/notebooks.
+The notebooks are put in $CLAW/doc/gallery/apps with the same subdirectory
+structure as in $CLAW/apps.
+
+As of v5.7.0:
+No longer assume they are in apps/notebooks, and put apps/etc in _static/ path.
 """
 
 from __future__ import print_function
@@ -13,34 +16,35 @@ import os
 
 try:
     CLAW = os.environ['CLAW']
-    apps_notebooks = os.path.join(CLAW,'apps','notebooks')
-    assert os.path.isdir(apps_notebooks), "*** apps repository missing?"
+    apps_dir = os.path.join(CLAW,'apps')
+    assert os.path.isdir(apps_dir), "*** apps repository missing?"
 except:
     print("$CLAW/apps must point to the apps repository")
 
-static_notebook_dir = os.path.join(CLAW,'doc','gallery','_static','notebooks')
+static_apps_dir = os.path.join(CLAW,'doc','gallery','_static','apps')
 
-cmd = 'mkdir -p %s' % static_notebook_dir
+cmd = 'mkdir -p %s' % static_apps_dir
 print('>>> %s' % cmd)
 os.system(cmd)
 
-notebooks = ['geoclaw/topotools_examples.ipynb',
-             'geoclaw/dtopotools_examples.ipynb',
-             'geoclaw/Okada.ipynb',
-             'geoclaw/chile2010a/chile2010a.ipynb',
-             'geoclaw/chile2010b/chile2010b.ipynb',
-             'amrclaw/advection_2d_square/amrclaw_advection_2d_square.ipynb',
-             'classic/advection_1d/advection_1d.ipynb',
-             'classic/acoustics_1d_example1/acoustics_1d_example1.ipynb']
+notebooks = ['notebooks/geoclaw/topotools_examples.ipynb',
+             'notebooks/geoclaw/dtopotools_examples.ipynb',
+             'notebooks/geoclaw/Okada.ipynb',
+             'notebooks/geoclaw/chile2010a/chile2010a.ipynb',
+             'notebooks/geoclaw/chile2010b/chile2010b.ipynb',
+             'notebooks/amrclaw/advection_2d_square/amrclaw_advection_2d_square.ipynb',
+             'notebooks/classic/advection_1d/advection_1d.ipynb',
+             'notebooks/classic/acoustics_1d_example1/acoustics_1d_example1.ipynb']
 
-#notebooks = ['classic/acoustics_1d_example1/acoustics_1d_example1.ipynb']
+# for testing:
+notebooks = ['notebooks/geoclaw/Okada.ipynb']
 
 
 for nb in notebooks:
     nbdir, nbname = os.path.split(nb)
-    input_file = os.path.join(apps_notebooks, nb)
-    output_file = os.path.join(static_notebook_dir, nb.replace('ipynb','html'))
-    cmd = 'mkdir -p %s' % os.path.join(static_notebook_dir, nbdir)
+    input_file = os.path.join(apps_dir, nb)
+    output_file = os.path.join(static_apps_dir, nb.replace('ipynb','html'))
+    cmd = 'mkdir -p %s' % os.path.join(static_apps_dir, nbdir)
     print('>>> %s' % cmd)
     os.system(cmd)
 
@@ -49,7 +53,7 @@ for nb in notebooks:
             "--execute",
             "--output", output_file,
             "--ExecutePreprocessor.kernel_name=python3",
-            "--ExecutePreprocessor.timeout=600", 
+            "--ExecutePreprocessor.timeout=1200", 
             input_file]
 
     cmd = ' '.join(args)
